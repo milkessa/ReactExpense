15a51ee8b24cb95ce217d424995ba59aea681cf2
milkessa <milkessagebi@gmail.com>
2022-12-07 21:22:13 +0300

    Updat

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,12 +1,25 @@
 import "./ExpenseForm.css";
 const expenseForm = () => {
   return (
-    <div>
-      <form
-        action='
-        '
-      ></form>
-    </div>
+    <form action=''>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label htmlFor=''>Title</label>
+          <input type='text' />
+        </div>
+        <div className='new-expense__control'>
+          <label htmlFor=''>Amount</label>
+          <input type='number' min='0.01' step='0.01' />
+        </div>
+        <div className='new-expense__control'>
+          <label htmlFor=''>Date</label>
+          <input type='date' min='2019-01-01' max='2022-02-09' />
+        </div>
+      </div>
+      <div className='new_expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
   );
 };
 
